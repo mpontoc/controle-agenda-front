3966a7af650e151de6752cb265c1a214b8aa23a7
mpontoc <mpontoc@hotmail.com>
2024-08-01 09:54:33 -0300

    upadate comunication done!

--- script.js
+++ script.js
@@ -1,58 +1,34 @@
-function getDataUser(e) {
+window.onload = function() {
+  var enviar = document.getElementById("enviar");
+  enviar.addEventListener("click", function() {
+    var user = document.getElementById("user").value;
+    var pass = document.getElementById("password").value;
+    createUser(user, pass);
+  });
+};
 
-    var user = document.getElementById('user')
-    var pass = document.getElementById('password')
-    var enviar = document.getElementById('enviar')
+async function createUser(user, pass) {
+  try {
+    let options = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    };
+  
+    var response = await fetch(
+      "http://localhost:8080/agendapessoal/users/2",
+      options
+    );
+  
+    var data = await response.json();
+    console.log(JSON.stringify(data));
+    console.log(data.usersName);
+  
+    alert("Espera o BANG");
 
-    try {
-
-        e.preventDefault()
-
-        var response = enviar.addEventListener('click', createUser(e, user.value, pass.value));
-
-        console.log(response)
-
-    } catch (error) {
-        console.log(error)
-
-    }
+    alert("Data " + JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-async function createUser(e, user, pass) {
-
-
-    e.preventDefault()
-
-    try {
-
-        const formData = new FormData()
-        formData.append('usersName', user)
-        formData.append('password', pass)
-
-        let options = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            mode: 'cors',
-            body: `{usersName: "${user}",password: "${pass}"}`
-            // body: '{"usersName":"usersNew1","password":"1234"}'
-        };
-
-        // var response = await fetch('https://controle-agenda.herokuapp.com/controleagenda/users', options)
-        var response = fetch('http://localhost:8080/controleagenda/users', options)
-        alert("Espera o BANG")
-        var responseConvertido = await response.json()
-        console.log(responseConvertido)
-        console.log(responseConvertido.usersName)
-    } catch (error) {
-        console.log(error)
-    }
-
-
-}
-
-
-
-
